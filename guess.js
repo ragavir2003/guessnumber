a15bef3bee6020d12d3e3c437dc82c92e44aa8b2
ragavir2003 <ragavimaha1819@gmail.com>
2023-10-24 10:05:30 +0530

    guess the number from 1 to 10 game

--- guess.js
+++ guess.js
@@ -1,4 +1,3 @@
-
 <html>
 <body>
 <h1>Guess the number from 1 to 10</h1>
@@ -6,22 +5,20 @@
 <button onclick="check()">Check</button>
 <p id="result">you are wrong/right</p>
 <p id="score">score:10</p>
-
 <script>
 var guessnumber=document.getElementById("guessnumber")
 var result=document.getElementById("result")
 var score=document.getElementById("score")
 var randomnumber=Math.floor(Math.random()*10)+1
 var totalscore=10
-function check()
 
+function check()
     var enterednumber=guessnumber.value
     if(randomnumber==enterednumber)
         {
         console.log("Right")
         result.textContent="Right" 
         alert("YOU WON..")
-    
         }
     else
      {
